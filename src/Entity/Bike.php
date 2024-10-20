b249ad83d8512d8366d0b44afe91e8833ee59a09
Lluís Fontboté <lluis.fontbote@outlook.com>
2024-10-20 23:41:01 +0200

    Add filters and ordering

--- src/Entity/Bike.php
+++ src/Entity/Bike.php
@@ -2,6 +2,10 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Common\Filter\SearchFilterInterface;
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
@@ -22,6 +26,22 @@ use Symfony\Component\Validator\Constraints as Assert;
 #[Patch(validationContext: ['groups' => ['Default', 'patchValidation']])]
 #[GetCollection]
 #[Post(validationContext: ['groups' => ['Default', 'postValidation']])]
+#[
+    ApiFilter(
+        SearchFilter::class,
+        properties: [
+            'model' => SearchFilterInterface::STRATEGY_PARTIAL,
+            'cylinderCapacity' => SearchFilterInterface::STRATEGY_EXACT,
+            'brand' => SearchFilterInterface::STRATEGY_PARTIAL,
+            'type' => SearchFilterInterface::STRATEGY_EXACT,
+            'weight' => SearchFilterInterface::STRATEGY_EXACT,
+        ]
+    ),
+    ApiFilter(
+        OrderFilter::class,
+        properties: ['id', 'model', 'brand', 'cylinderCapacity', 'weight', 'type'],
+    ),
+]
 class Bike
 {
     #[ORM\Id]
